c775a74c32e8c4deb6d3dbe80313238ce68c45c9
jpaltahona <jpaltahonacc@gmail.com>
2019-11-16 11:56:41 -0500

    add routes aut

--- src/routes/auth.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-//import { login } from '../controllers/login.controllers';
-const Authentications = Router();
-
-Authentications.route('/')
-   // .post(login);
-
-export default Authentications;
--- src/server.js
+++ src/server.js
@@ -1,69 +1,22 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { OAuth } from  'oauth' ;
-import Authentications from './routes/auth'
+import sessions from './routes/session'
+import Auth from './auth/index';
+import cookieParser from 'cookie-parser';
+//import session from 'express-session';
 
 const app = express();
+
+//middleware
 app.use( bodyParser.urlencoded({  extended: false }));
 app.use(bodyParser.json());
 app.use(express.json());
+app.use(cookieParser());
 
-    var oauth = new OAuth(
-            "https://api.twitter.com/oauth/request_token",
-            "https://api.twitter.com/oauth/access_token",
-            "5itLbT4POJAf8TILbB02zgbjU",
-            "6yKUX6FVi6AIvd1Rv9f3L7dmZzZvkKdKIfGW6o6pQBw71siTpe",
-            "1.0",
-            "http://localhost:8080/sessions/callback",
-            "HMAC-SHA1"
-    );
 
-    app.get('/auth/twitter', (req, res) => {
-       oauth.getOAuthRequestToken( (error, oauth_token, oauth_token_secret, results) => {
-            if(error){
-                console.log(error);
-                res.send("Authentication Failed!");
-            }
-            else{
-                req.session = {
-                    token: oauth_token,
-                    token_secret: oauth_token_secret
-                };
-                console.log(req.session);
-                res.redirect(`https://twitter.com/oauth/authenticate?oauth_token=${oauth_token}` )
-            }
-       });
-    });
-
-    app.get('/sessions/callback', (req, res, next) => {
-        if(req.session.oauth){
-            req.session.oauth.verifier = req.query.oauth_verifier;
-            const oauth_data = req.session.oauth;
-
-            oauth.getOAuthRequestToken(
-                oauth_data.token,
-                oauth_data.token_secret,
-                oauth_data.verifier,
-                (error, oauth_access_token, oauth_access_token_secret, results) => {
-                    if (error) {
-                        console.log(error);
-                        res.send("Authentication Failure!");
-                      }
-                    else {
-                        eq.session.oauth.access_token = oauth_access_token;
-                        req.session.oauth.access_token_secret = oauth_access_token_secret;
-                        console.log(results, req.session.oauth);
-                        res.send("Authentication Successful");
-                        // res.redirect('/');
-                    }
-                }
-            )
-        }
-        else {
-            res.redirect('/login');
-        }
-    });
 // Routes
+app.use('/auth/twitter', Auth);
+app.use('/sessions/callback', sessions);
 
 app.listen(8080, function() {
   console.log('App runining on port 8080!');
